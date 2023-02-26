7e55f8d86b57c420b5bb57fc5d6573068e01e2f4
Bakary Sylla <syllabakary2002@gmail.com>
2023-02-26 05:38:52 -0800

    bugg fix

--- sanity_ecommerce/schemas/index.js
+++ sanity_ecommerce/schemas/index.js
@@ -1,25 +1,7 @@
-//First import she schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
-
-//Import schema types from any pluging that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
-
 //import our document product and banner
 import product from './product'
 import banner from './banner'
 
-//give our schema to the builder and provide the result to sanity
+//call you import inside the schemaType
 
-export default createSchema({
-  //name our schema
-  name: 'default',
-  //then proceed to concatenate our document type
-  //to the ones provided by any pluging that are installed
-  types: schemaTypes.concat([
-    //add our document here
-    product,
-    banner,
-  ]),
-})
-
-// export const schemaTypes = []
+export const schemaTypes = [product, banner]
