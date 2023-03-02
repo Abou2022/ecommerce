e0e386944c4963dcd67d950d8f088f35c06e960b
Bakary Sylla <syllabakary2002@gmail.com>
2023-03-02 11:00:22 -0800

    Footer added with icons

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import {
+  AiFillInstagram,
+  AiOutlineTwitter,
+  AiOutlineFacebook,
+} from "react-icons/ai";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container">
+      <p> 2023 AfricaBazin Store All Rights Reserverd</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineFacebook />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
