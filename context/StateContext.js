24af3deb1490fb8cbf9f37b137f2d45f785f9306
Bakary Sylla <syllabakary2002@gmail.com>
2023-03-13 11:36:29 -0700

    stateContext update

--- context/StateContext.js
+++ context/StateContext.js
@@ -5,5 +5,16 @@ const Context = createContext();
 
 export const StateContext = ({ children }) => {
   const [showCart, setShowCart] = useState(false);
-  const [cartItems, setCartItems] = useState(false);
+  const [cartItems, setCartItems] = useState();
+  const [totalPrice, setTotalPrice] = useState();
+  const [totalQuantities, setTotalQuantities] = useState();
+  const [qty, setQty] = useState(1);
+
+  return (
+    <Context.Provider
+      value={{ showCart, cartItems, totalPrice, totalQuantities, qty }}
+    >
+      {children}
+    </Context.Provider>
+  );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,18 @@
 import "@/styles/globals.css";
 import "../styles/Home.module.css";
 import React from "react";
+import { Toaster } from "react-hot-toast";
 
 import { Layout } from "@/components";
+import { StateContext } from "@/context/StateContext";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   );
 }
