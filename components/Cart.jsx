3fbc8eb7bc84503d2326c8e68dd7d4c5d1449b57
Bakary Sylla <syllabakary2002@gmail.com>
2023-03-23 12:30:21 -0700

    shoppind cart update

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -8,9 +8,34 @@ import {
 } from "react-icons/ai";
 import { TiDeleteOutline } from "react-icons/ti";
 import toast from "react-hot-toast";
+import { useStateContext } from "@/context/StateContext";
+import { urlFor } from "@/lib/client";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const cartRef = useRef();
+  const { totalPrice, totalQuantities, cartItems, setShowCart } =
+    useStateContext();
+
+  return (
+    <div className="cart-wrapper" ref={cartItems}>
+      <div className="cart-container">
+        <button
+          type="button"
+          className="cart-heading"
+          onClick={() => setShowCart(false)}
+        >
+          <AiOutlineLeft />
+          <span className="heading">Your Cart</span>
+          <span className="cart-num-items">({totalQuantities} items)</span>
+        </button>
+        {cartItems.length < 1 && (
+          <div className="empty-cart">
+            <AiOutlineShopping size={150} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
