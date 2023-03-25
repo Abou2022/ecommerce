e41dcf158399432b79a7488b110c69648810c976
Bakary Sylla <syllabakary2002@gmail.com>
2023-03-24 17:02:22 -0700

    shipping rate added

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -5,7 +5,15 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
       // Create Checkout Sessions from body params.
-      const session = await stripe.checkout.sessions.create({
+      const params = {
+        submit_type: "pay",
+        mode: "payment",
+        payment_method_types: ["card"],
+        billing_address_collection: "auto",
+        shipping_options: [
+          { shipping_rate: process.env.STRIP_NORMAL_SHIPPING },
+          { shipping_rate: process.env.STRIP_FAST_SHIPPING },
+        ],
         line_items: [
           {
             // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
@@ -16,7 +24,10 @@ export default async function handler(req, res) {
         mode: "payment",
         success_url: `${req.headers.origin}/?success=true`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
-      });
+      };
+
+      const session = await stripe.checkout.sessions.create(params);
+
       res.redirect(303, session.url);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
