836604bf491ad10f48c69d947602bf58cf5ad4df
Bakary Sylla <syllabakary2002@gmail.com>
2023-03-26 17:18:53 -0700

    Stripe update

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -22,6 +22,13 @@ export default async function handler(req, res) {
             quantity: 1,
           },
         ],
+        line_items: req.body.cartItems.map((item) => {
+          const img = item.image[0].asset._ref;
+          const newImage = img.replace(
+            "image-",
+            "https://cdn.sanity.io/images/"
+          );
+        }),
         mode: "payment",
         success_url: `${req.headers.origin}/?success=true`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
