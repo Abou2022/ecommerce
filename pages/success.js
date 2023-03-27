e6ca466c70bacf1812206965308f0b6163963497
Bakary Sylla <syllabakary2002@gmail.com>
2023-03-27 15:27:46 -0700

    bsBagC added

--- pages/success.js
+++ pages/success.js
@@ -6,7 +6,17 @@ import { useRouter } from "next/router";
 import { useStateContext } from "@/context/StateContext";
 
 const success = () => {
-  return <div>success</div>;
+  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+  const [order, setOrder] = useState(null);
+  return (
+    <div className="success-wrapper">
+      <div className="success">
+        <p className="icon">
+          <BsBagCheckFill />
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default success;
