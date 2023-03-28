2a031c682a9a263d887fcb9298e8af86980e2eed
Bakary Sylla <syllabakary2002@gmail.com>
2023-03-27 17:08:29 -0700

    success update

--- pages/success.js
+++ pages/success.js
@@ -14,6 +14,19 @@ const success = () => {
         <p className="icon">
           <BsBagCheckFill />
         </p>
+        <h2>Thank you for your order!</h2>
+        <p className="email-msg">Check your email inbox for the receipt.</p>
+        <p className="description">
+          If you have any questions, please email
+          <a className="email" href="mailto:africainbazin225@gmail.com">
+            africainbazin225@gmail.com
+          </a>
+        </p>
+        <Link href="/">
+          <button type="button" width="300px" className="btn">
+            Continue Shopping
+          </button>
+        </Link>
       </div>
     </div>
   );
